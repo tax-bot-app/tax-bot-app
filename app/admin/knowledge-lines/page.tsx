634160c8072e9fd4fad14f8e17e59f637ff6dc2f
tax-bot-appi <s-noguchi@gladplan.com>
2026-01-24 16:37:14 +0900

    fix: make knowledge-lines page a module

---
+++ app/admin/knowledge-lines/page.tsx
@@ -0,0 +1,6 @@
+// app/admin/knowledge-lines/page.tsx
+import KnowledgeLinesClient from "./ui/KnowledgeLinesClient";
+
+export default function KnowledgeLinesPage() {
+  return <KnowledgeLinesClient />;
+}
