76baf85ebfb32acc00a5777e350f3733ec66e0ce
tax-bot-appi <s-noguchi@gladplan.com>
2026-03-16 13:22:23 +0900

    google tag　追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Yuji_Syuku } from "next/font/google";
 import "./globals.css";
@@ -26,6 +27,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Suspense fallback={null}>
           <MetaPixel />
         </Suspense>
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-769471741"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-769471741');
+          `}
+        </Script>
+
         {children}
       </body>
     </html>
